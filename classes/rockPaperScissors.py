d1e667dd83366ec60ab18f905cc75e08a5d2d013
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-06-24 21:46:45 -0300

    rock paper scissors

--- classes/rockPaperScissors.py
+++ classes/rockPaperScissors.py
@@ -28,46 +28,46 @@ paper = '''
 '''
 
 user = int(input('Please type 0 for Rock, 1 for Paper and 2 for Scissors\n'))
-if user == 0:
-    print(rock)
-elif user == 1:
-    print(paper)
+
+if user >= 3 or user < 0:
+    print("Invalid Input")
 else:
-    print(scissors)
+    if user == 0:
+        print(rock)
+    elif user == 1:
+        print(paper)
+    else:
+        print(scissors)
 
-pc = random.randint(0, 2)
+    pc = random.randint(0, 2)
 
-if pc == 0:
-    print('Computer chose: ', rock)
-elif pc == 1:
-    print('Computer chose: ', paper)
-else:
-    print('Computer chose: ', scissors)
-
-
-if user == 0:  # Rock
     if pc == 0:
-        print('Tie game')
+        print('Computer chose: ', rock)
     elif pc == 1:
-        print('You lose')
-    elif pc == 2:
-        print('You win')
+        print('Computer chose: ', paper)
+    else:
+        print('Computer chose: ', scissors)
 
-elif user == 1:  # Paper
-    if pc == 0:
-        print('You win')
-    elif pc == 1:
-        print('Tie game')
-    elif pc == 2:
-        print('You lose')
+    if user == 0:  # Rock
+        if pc == 0:
+            print('Tie game')
+        elif pc == 1:
+            print('You lose')
+        elif pc == 2:
+            print('You win')
 
-elif user == 2:  # Scissors
-    if pc == 0:
-        print('You lose')
-    elif pc == 1:
-        print('You win')
-    elif pc == 2:
-        print('Tie game')
+    elif user == 1:  # Paper
+        if pc == 0:
+            print('You win')
+        elif pc == 1:
+            print('Tie game')
+        elif pc == 2:
+            print('You lose')
 
-else:
-    print("Invalid choice")
+    elif user == 2:  # Scissors
+        if pc == 0:
+            print('You lose')
+        elif pc == 1:
+            print('You win')
+        elif pc == 2:
+            print('Tie game')
