6c92b364a7bfaf2f802f39c427c007fbec9cc1a7
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-06-27 22:58:51 -0300

    add second solution for loop

--- classes/56-addingEvenNumbers.py
+++ classes/56-addingEvenNumbers.py
@@ -1,8 +1,18 @@
+# number = 0
+# target = int(input('type a number\n'))
+
+# for i in range(0, target + 1):
+#     if i % 2 == 0:
+#         number += i
+
+# print(number)
+
+# Another solution
+
 number = 0
 target = int(input('type a number\n'))
 
-for i in range(0, target + 1):
-    if i % 2 == 0:
-        number += i
+for i in range(2, target + 1, 2):
+    number += i
 
 print(number)
