5fddb324b865062bd4244a7d836b363e3c568694
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-06-28 21:12:48 -0300

    for loop

---
+++ classes/58-passwordGenerator.py
@@ -0,0 +1,7 @@
+
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+number_letters = int(input('How many number of letters in your password?\n'))
+number_symbols = int(input('How many symbols would you like?\n'))
+number_numbers = int(input('How many numbers would you like?\n'))
