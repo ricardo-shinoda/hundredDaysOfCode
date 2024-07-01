95e1a1e53c77e3f4319740ff4699c48cb8686739
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-06-30 22:03:57 -0300

    while loop

---
+++ classes/61-callingFunction.py
@@ -0,0 +1,6 @@
+def stratocaster():
+    print('My dream stratocaster is an original 56')
+    print('But could also be a 59')
+
+
+stratocaster()
---
+++ classes/64-whileLoop.py
@@ -0,0 +1,18 @@
+number_lap = 10
+
+while number_lap > 0:
+    print("one more lap")
+    number_lap -= 1
+
+print("Outside the while loop")
+
+# How to use a negative:
+
+
+at_goal = True
+
+while not at_goal():
+    at_goal = False
+
+    # while at_goal != True:
+    # run the code
