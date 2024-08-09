13d85424ddc4203b10685bbbc1b8c4659128d905
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-08-09 20:51:30 -0300

    start lesson 63-ceaser cipher

--- classes/63-caesarCipher.py
+++ classes/63-caesarCipher.py
@@ -1,10 +1,10 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input(
-    "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# direction = input(
+#     "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1  create a function called encrypt() that takes original_text and shift_amount as 2 inputs
