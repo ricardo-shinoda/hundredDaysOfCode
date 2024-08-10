8ea8dec6f3b44872a251253deabe4c50f0298771
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-08-10 18:10:03 -0300

    starting with decode function

--- classes/63-caesarCipher.py
+++ classes/63-caesarCipher.py
@@ -3,25 +3,25 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 direction = input(
     "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-new_alphabet = []
+encode_text = []
 # TODO-2  create a function called encrypt() that takes original_text and shift_amount as 2 inputs
 
-
-def encrypt(original_text, shift_amount):
-    for i in original_text:
-        position = alphabet.index(i)
-        target = (position + shift_amount) % len(alphabet)
-        value = alphabet[target]
-        new_alphabet.append(value)
-        text_output = ''.join(new_alphabet)
-
-    print(text_output)
-
-
 if direction == "encode":
-    encrypt(text, shift)
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def encrypt(original_text, shift_amount):
+        for i in original_text:
+            position = alphabet.index(i) + shift_amount
+            # return remainer so i counst only the last turn
+            target = position % len(alphabet)
+            value = alphabet[target]
+            encode_text.append(value)
+            text_output = ''.join(encode_text)
+
+        print(f'This is the encode output: {text_output}')
+
+    encrypt(original_text=text, shift_amount=shift)
 else:
     print("Need to finish this code")
