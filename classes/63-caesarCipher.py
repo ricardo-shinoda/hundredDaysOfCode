068ce90ee492aadec2d44e2f69452378e60e6c5d
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-08-10 09:19:45 -0300

    solution on ceaser cihper

--- classes/63-caesarCipher.py
+++ classes/63-caesarCipher.py
@@ -6,12 +6,27 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 
-
+new_alphabet = []
 # TODO-1  create a function called encrypt() that takes original_text and shift_amount as 2 inputs
 
+
 def encrypt(original_text, shift_amount):
-    print(f'This is original_text {original_text}')
-    print(f'This is shift_amount {shift_amount}')
+    # text = input(f'This is original_text {original_text}')
+    # amount = input(f'This is shift_amount {shift_amount}')
+    # print(text)
+    # print(amount)
+    for i in original_text:
+        # print(i)
+        # position = (i).index()
+        position = alphabet.index(i)
+        target = position + shift_amount
+        # print(position)
+        # print(target)
+        test = alphabet[target]
+        new_alphabet.append(test)
+        # print(test)
+
+    print(new_alphabet)
 
 
 encrypt('hello', 1)
