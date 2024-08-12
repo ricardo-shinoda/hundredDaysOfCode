7559f3df771cfb65a072ea800b41eb1138ba1555
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-08-11 21:57:59 -0300

    encode and decode

--- classes/63-caesarCipher.py
+++ classes/63-caesarCipher.py
@@ -3,14 +3,14 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 direction = input(
     "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 encode_text = []
+decode_text = []
 # TODO-2  create a function called encrypt() that takes original_text and shift_amount as 2 inputs
 
 if direction == "encode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-
     def encrypt(original_text, shift_amount):
         for i in original_text:
             position = alphabet.index(i) + shift_amount
@@ -23,5 +23,16 @@ if direction == "encode":
         print(f'This is the encode output: {text_output}')
 
     encrypt(original_text=text, shift_amount=shift)
-else:
-    print("Need to finish this code")
+
+elif direction == "decode":
+    def decrypt(original_text, shift_amout):
+        for i in original_text:
+            position = alphabet.index(i) - shift_amout
+            target = position % len(alphabet)
+            value = alphabet[target]
+            decode_text.append(value)
+            text_output = ''.join(decode_text)
+
+        print(f'This is the decode output: {text_output}')
+
+    decrypt(original_text=text, shift_amout=shift)
