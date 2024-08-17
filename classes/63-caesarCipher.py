e9ed49faf9b104f24d98f6fdf95e662db694ba6a
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-08-17 18:43:19 -0300

    improving the code

--- classes/63-caesarCipher.py
+++ classes/63-caesarCipher.py
@@ -4,38 +4,46 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 direction = input(
     "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
-if text in alphabet:
-    shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number:\n"))
 
-    def caesar(direction_imput, text_imput, shift_imput):
-        encode_text = []
-        decode_text = []
-        if direction_imput == "encode":
-            def encrypt():
-                for i in text_imput:
+
+def caesar(direction_imput, text_imput, shift_imput):
+    encode_text = []
+    decode_text = []
+    coringa = []
+    if direction_imput == "encode":
+        def encrypt():
+            for i in text_imput:
+                if i in alphabet:
                     position = alphabet.index(i) + shift_imput
                     # return remainer so it counts only the last turn
                     target = position % len(alphabet)
                     value = alphabet[target]
                     encode_text.append(value)
                     text_output = ''.join(encode_text)
+                else:
+                    coringa.append(i)
 
+                    coringa_output = ''.join(coringa)
+
+            if i in alphabet:
                 print(f'This is the encode output: {text_output}')
-            encrypt()
+            else:
+                print(f'This is the fixed code: {coringa_output}')
 
-        elif direction_imput == "decode":
-            def decrypt():
-                for i in text_imput:
-                    position = alphabet.index(i) - shift_imput
-                    target = position % len(alphabet)
-                    value = alphabet[target]
-                    decode_text.append(value)
-                    text_output = ''.join(decode_text)
+        encrypt()
 
-                print(f'This is the decode output: {text_output}')
-            decrypt()
+    elif direction_imput == "decode":
+        def decrypt():
+            for i in text_imput:
+                position = alphabet.index(i) - shift_imput
+                target = position % len(alphabet)
+                value = alphabet[target]
+                decode_text.append(value)
+                text_output = ''.join(decode_text)
 
-    caesar(direction_imput=direction, text_imput=text, shift_imput=shift)
+            print(f'This is the decode output: {text_output}')
+        decrypt()
 
-else:
-    print('Try again with right characters')
+
+caesar(direction_imput=direction, text_imput=text, shift_imput=shift)
