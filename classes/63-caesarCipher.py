2b807532e67378d315a62a83ff3538998a3180c4
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-08-29 20:43:31 -0300

    fix looping until I choose to get out

--- classes/63-caesarCipher.py
+++ classes/63-caesarCipher.py
@@ -30,19 +30,26 @@ def caesar(direction_imput, text_imput, shift_imput):
     elif direction_imput == "decode":
         def decrypt():
             for i in text_imput:
-                position = alphabet.index(i) - shift_imput
-                target = position % len(alphabet)
-                value = alphabet[target]
-                decode_text.append(value)
-                text_output = ''.join(decode_text)
+                if i in alphabet:
+                    position = alphabet.index(i) - shift_imput
+                    target = position % len(alphabet)
+                    value = alphabet[target]
+                    decode_text.append(value)
+                    text_output = ''.join(decode_text)
+                else:
+                    decode_text.append(i)
 
             print(f'This is the decode output: {text_output}')
         decrypt()
 
     continue_encoding = input(
-        "Type 'Yes' if you want to continue encoding/decoding words, otherwise type 'No'")
+        "Type 'Yes' if you want to continue encoding/decoding words, otherwise type 'No'\n").lower()
 
-    if continue_encoding == 'Yes':
+    if continue_encoding == 'yes':
+        direction = input(
+            "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
         caesar(direction_imput=direction, text_imput=text, shift_imput=shift)
     else:
         print("This is the end of the encoding/decoding\n")
