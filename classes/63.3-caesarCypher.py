2b5b42b12e9305fb3e876e6aa0c4f24dedf13fb9
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-08-29 21:18:11 -0300

    add the initial text

--- classes/63.3-caesarCypher.py
+++ classes/63.3-caesarCypher.py
@@ -1,5 +1,10 @@
+from art import text2art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+initial_text = text2art('Caesar Cypher', font='standard')
+print(initial_text)
 
 direction = input(
     "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
