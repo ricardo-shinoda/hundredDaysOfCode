5a666393916f8acc0a9ba5fc3cd72396c1126117
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-09-04 21:33:09 -0300

    starting calculator program

--- classes/73-functionsOutput.py
+++ classes/73-functionsOutput.py
@@ -1,4 +1,7 @@
 def format_name(f_name, l_name):
+    # this is a docstring, must always be on the first idented line inside a function
+    """Take a first and last name and format it to
+    return the title case version of the name"""
     first_cap = f_name.title()
     last_cap = l_name.title()
     return f'{first_cap} {last_cap}'
