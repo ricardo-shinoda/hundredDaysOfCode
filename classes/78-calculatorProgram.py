79cd858240c2133b71b2919e014dec929ca94211
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-09-05 21:48:43 -0300

    updating calculator

--- classes/78-calculatorProgram.py
+++ classes/78-calculatorProgram.py
@@ -12,3 +12,16 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+
+# Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+
+calc = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+print(calc["+"](1, 3))
