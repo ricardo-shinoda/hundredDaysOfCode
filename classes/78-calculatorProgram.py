6dadcaf3f857f0887bb7311e9696f0150f8e4df7
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-09-09 21:41:41 -0300

    calculator finished

--- classes/78-calculatorProgram.py
+++ classes/78-calculatorProgram.py
@@ -1,3 +1,7 @@
+from os import system
+from art import *
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -26,34 +30,31 @@ calc = {
 # program asks the user to type the first number
 
 
-def first_count():
-    first_number = int(input("What is the first number?\n"))
-    operator = input(
-        "Chose one mathematical operator?\n+\n-\n*\n/\n")
-    second_number = int(input("What is the second number?\n"))
-    return calc[operator](first_number, second_number)
+def calculator():
+    label = text2art("Calculator")
+    print(label)
+
+    should_accumulate = True
+    first_number = float(input("What is the first number?\n"))
+
+    while should_accumulate:
+        for i in calc:
+            print(i)
+
+        operator = input("What is the mathematical operator?\n")
+        second_number = float(input("What is the second number?\n"))
+        answer = calc[operator](first_number, second_number)
+        print(f"{first_number} {operator} {second_number} = {answer}")
+
+        choice = input(
+            f"Type: 'yes' to continue calculating with {answer}, or 'not'to start all over again\n")
+
+        if choice == "yes":
+            first_number = answer
+        else:
+            should_accumulate = False
+            system("clear")
+            calculator()
 
 
-result = first_count()
-print(f'this is result: ', result)
-
-cont_previous = input(
-    "Do you want to continue using the previous result?\n").lower()
-
-if cont_previous == 'yes':
-
-    def second_count():
-        second = result
-        operator = input("Chose one mathematical operator?\n+\n-\n*\n/\n")
-        second_number = int(input("What is the second number?\n"))
-        return calc[operator](second, second_number)
-
-    output = second_count()
-    second = output
-    print(f'this is output: ', output)
-
-elif cont_previous == 'no':
-    rerun = first_count()
-    # print(loop_again)
-
-cont_previous = input('do you want to continure the loop?\n')
+calculator()
