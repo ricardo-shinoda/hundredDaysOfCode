4baef667f1d76d0e80ca37452b2803c34d7c6172
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-09-11 14:48:43 -0300

    blackjack, adding to my sum is working

--- classes/79-blackJack.py
+++ classes/79-blackJack.py
@@ -1,3 +1,40 @@
+import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-start = input("Do you want to play a game of Blackjack? Type 'y'or 'n':\n")
+
+def black_jack():
+
+    play_again = True
+    start = input(
+        "Do you want to play a game of Blackjack? Type 'y' or 'n':\n")
+
+    if start == 'y':
+
+        result1 = random.choice(cards)
+        result2 = random.choice(cards)
+        my_cards = [result1, result2]
+        my_sum = sum(my_cards)
+        computer_card = random.choice(cards)
+
+    print(f'Your cards: ', my_cards, 'current score is: ', my_sum)
+    print(f"Computer's first card: ", computer_card)
+
+    while play_again:
+
+        result1 = random.choice(cards)
+        result2 = random.choice(cards)
+        computer_card = random.choice(cards)
+
+        again = input('Type "y" to get another card, type "n" to pass:\n')
+
+        if again == 'y':
+            new_number = random.choice(cards)
+            my_cards.append(new_number)
+            print(f'Your cards: ', my_cards,
+                  'current score is: ', sum(my_cards))
+        else:
+            play_again = False
+
+
+black_jack()
