1e24eb18a503740cf6c3c9d5da25804d15ecb995
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-09-21 08:59:19 -0300

    Number Guessing project

--- classes/91-numberGuessingGame.py
+++ classes/91-numberGuessingGame.py
@@ -1,8 +1,35 @@
 
+import random
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
-leve = input("Choose a difficulty. Type 'easy' or 'hard':\n")
+level = input("Choose a difficulty. Type 'easy' or 'hard':\n")
 
 easy = 10
 hard = 5
+rounds = ""
+
+choosen = int(random.choice(range(1, 100)))
+
+if level == 'hard':
+    rounds = 5
+    # print(f"You have {rounds} atempts remaining to guess the number")
+elif level == 'easy':
+    rounds = 10
+    # print(f"You have {rounds} atempts remaining to guess the number")
+
+
+while rounds > 0:
+    print(f"You have {rounds} attempts remaining to guess the number.")
+    tentative = int(input("Make a guess: "))
+    print(choosen)
+    if tentative > choosen:
+        print("Too high.")
+        rounds -= 1
+    elif tentative < choosen:
+        print('Too low.')
+        rounds -= 1
+    elif tentative == choosen:
+        print(f'You got it! The answer was {tentative}')
+        rounds = 0
+print("End of the game")
--- classes/91-toDoList.md
+++ classes/91-toDoList.md
@@ -5,10 +5,10 @@ hard = 5 attempts
 
 ________________________________________________
 
-1 - Add the initials print statements
-2 - Add a random.choise betwewn 1 and 100 and save into a variable
-3 - Add an if statement with the level of the game that is going to be played
-4 - Print the number of initial attempts
-5 - Add an input with "Make a guess: "
-6 - Maybe add an for loop with the number of guesses chosen on the level of the game
-7 - Add an if statement saying that is "Too high", "Too low" or "You got it! The answer was xx." 
+1 - [x] Add the initials print statements
+2 - [x] Add a random.choise betwewn 1 and 100 and save into a variable
+3 - [x] Add an if statement with the level of the game that is going to be played
+4 - [ ] Print the number of initial attempts
+5 - [ ] Add an input with "Make a guess: "
+6 - [ ] Maybe add an for loop with the number of guesses chosen on the level of the game
+7 - [ ] Add an if statement saying that is "Too high", "Too low" or "You got it! The answer was xx." 
