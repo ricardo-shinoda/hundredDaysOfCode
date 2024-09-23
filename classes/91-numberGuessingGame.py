57c242127e2d39fb88b7f6b82fd987f65d4378d7
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-09-23 20:48:33 -0300

    refactor numbere guessing game

--- classes/91-numberGuessingGame.py
+++ classes/91-numberGuessingGame.py
@@ -1,6 +1,9 @@
 
-import random
+from random import randint
+from art import *
 
+label = text2art("Guessing Number Game")
+print(label)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 level = input("Choose a difficulty. Type 'easy' or 'hard':\n")
@@ -9,27 +12,26 @@ easy = 10
 hard = 5
 rounds = ""
 
-choosen = int(random.choice(range(1, 100)))
+choosen = randint(1, 100)
+
 
 if level == 'hard':
     rounds = 5
-    # print(f"You have {rounds} atempts remaining to guess the number")
 elif level == 'easy':
     rounds = 10
-    # print(f"You have {rounds} atempts remaining to guess the number")
 
-
-while rounds > 0:
-    print(f"You have {rounds} attempts remaining to guess the number.")
-    tentative = int(input("Make a guess: "))
-    print(choosen)
-    if tentative > choosen:
-        print("Too high.")
-        rounds -= 1
-    elif tentative < choosen:
-        print('Too low.')
-        rounds -= 1
-    elif tentative == choosen:
-        print(f'You got it! The answer was {tentative}')
-        rounds = 0
-print("End of the game")
+while rounds == 5 or rounds == 10:
+    while rounds > 0:
+        print(f"You have {rounds} attempts remaining to guess the number.")
+        tentative = int(input("Make a guess: "))
+        print(choosen)
+        if tentative > choosen:
+            print("Too high.")
+            rounds -= 1
+        elif tentative < choosen:
+            print('Too low.')
+            rounds -= 1
+        elif tentative == choosen:
+            print(f'You got it! The answer was {tentative}')
+            rounds = 0
+    print("End of the game")
