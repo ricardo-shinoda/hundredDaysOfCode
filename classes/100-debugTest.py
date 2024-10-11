3a49446ec53cf0785dfd533835320f58e6f70ea4
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-10-11 20:13:14 -0300

    ¨debug

---
+++ classes/100-debugTest.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+result = is_leap(2000)
+print(result)
