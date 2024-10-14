786bbc28e50e7f2b81fbca5d3e1d08236e27b4df
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-10-14 20:33:21 -0300

    higher lower game

---
+++ classes/102-higherLower.py
@@ -0,0 +1,17 @@
+
+# make a list of dice emojis
+
+# create an random number search
+
+# compare dice value against previous value
+
+
+# What should be printed:
+# logo1
+
+# compare a:
+
+# VS logo
+
+# agains b:
+# who has more followers? Tpe a or b:
