90fd94bd4e2b15370e0db721dd300cf0d922ebf8
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-10-16 20:23:34 -0300

    major funcionality is done, need to do the loop while winning

--- classes/102-higherLower.py
+++ classes/102-higherLower.py
@@ -11,19 +11,62 @@ import gameData
 
 # create an random number search
 first_pick = randint(0, 49)
-print(first_pick)
+# print(first_pick)
+first_one = gameData.data[first_pick]
+result_one = first_one['follower_count']
 
 second_pick = randint(0, 49)
-print(second_pick)
+second_one = gameData.data[second_pick]
+result_two = second_one['follower_count']
+# print(second_pick)
 
+winning = True
 # create a compare A
-path = gameData.data[first_pick]
-print(f"Compare A: {path['name']}, a {path['description']}, from {path['country']}")
-# print(path['name'])
+while winning:
+    def keep_playing(first_choice, second_choice):
+        path_a = gameData.data[first_choice]
+        # path_a = first_pick
+        print(f"Compare A: {path_a['name']}, a {path_a['description']}, from {path_a['country']}")
+        followers_a = path_a['follower_count']
+        print(followers_a)
+        # print(followers_a)
+        # print(path['name'])
 
-# create a compare b
-path = gameData.data[second_pick]
-print(f"Compare B: {path['name']}, a {path['description']}, from {path['country']}")
+        # create a compare b
+        path_b = gameData.data[second_choice]
+        # path_b = second_pick
+        print(f"Compare B: {path_b['name']}, a {path_b['description']}, from {path_b['country']}")
+        followers_b = path_b['follower_count']
+        # print(followers_b)
+        print(followers_b)
+
+        guess = input("Who has more followers? Type 'A' or 'B': ")
+
+        return guess
+
+    user_guess = keep_playing(first_choice=first_pick, second_choice=second_pick)
+    print(f'THIS IS USER_GURSS: {user_guess}')
+
+    if user_guess == 'A' and result_one > result_two:
+        print('You are winning!')
+        # first_choice = second_pick
+    elif user_guess == 'B' and result_two > result_one:
+        print('You are winning!')
+        # first_choice = second
+    else:
+        print('You loose...')
+        winning = False
+    
+# return guess
+    
+
+# winning = True
+
+# while winning:
+#         keep_playing(first_choice=first_pick, second_choice=second_pick)
+# winning = False
+
+
 
 #! To do list
 # make a list of dice emojis
