f2af7f6b5c32f99a5ce7a46a679eb65d7b8cffdd
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-10-16 20:58:15 -0300

    improvements on changing values when choose right

--- classes/102-higherLower.py
+++ classes/102-higherLower.py
@@ -1,14 +1,5 @@
 from random import randint
 import gameData
-
-# from art import *
-
-# logo1 = text2art("Higher or Lower")
-# print(logo1)
-
-# logo2 = text2art("VS")
-# print(logo2)
-
 # create an random number search
 first_pick = randint(0, 49)
 # print(first_pick)
@@ -29,10 +20,7 @@ while winning:
         print(f"Compare A: {path_a['name']}, a {path_a['description']}, from {path_a['country']}")
         followers_a = path_a['follower_count']
         print(followers_a)
-        # print(followers_a)
-        # print(path['name'])
-
-        # create a compare b
+    
         path_b = gameData.data[second_choice]
         # path_b = second_pick
         print(f"Compare B: {path_b['name']}, a {path_b['description']}, from {path_b['country']}")
@@ -40,7 +28,7 @@ while winning:
         # print(followers_b)
         print(followers_b)
 
-        guess = input("Who has more followers? Type 'A' or 'B': ")
+        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 
         return guess
 
@@ -48,47 +36,13 @@ while winning:
     print(f'THIS IS USER_GURSS: {user_guess}')
 
     if user_guess == 'A' and result_one > result_two:
-        print('You are winning!')
-        # first_choice = second_pick
+        print('You are winning! result 1 bigger then result two')
+        first_pick = first_one
     elif user_guess == 'B' and result_two > result_one:
-        print('You are winning!')
-        # first_choice = second
+        print('You are winning! result 2 bigger then result 1')
+        first_pick = second_pick
+        second_pick = int(randint(0, 49))
     else:
         print('You loose...')
         winning = False
     
-# return guess
-    
-
-# winning = True
-
-# while winning:
-#         keep_playing(first_choice=first_pick, second_choice=second_pick)
-# winning = False
-
-
-
-#! To do list
-# make a list of dice emojis
-# compare dice value against previous value
-# What should be printed:
-# make the logo1
-# make the logo 2
-
-#! Layout 1 round
-# logo1
-# compare a:
-# VS logo
-# agains b:
-# who has more followers? Tpe a or b:
-
-#! layout 2 round
-# logo 1
-# result (right/wrong) - current score ( += 1)
-# -- if is wrong:
-# ---- Logo 1
-# ---- "Sorry, that's wrong. Final score: xx"
-# Compare A = previous compare B
-# Vs logo
-# Against B:
-# Who has more followers?
