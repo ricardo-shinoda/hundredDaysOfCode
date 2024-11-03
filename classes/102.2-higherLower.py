bfa5e533c88b6be44acca23c05aeb24cb0d81167
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-11-03 08:04:07 -0300

    get description

--- classes/102.2-higherLower.py
+++ classes/102.2-higherLower.py
@@ -3,8 +3,21 @@ import gameData
 
 
 def gen_number():
+    """Generate a random number"""
     random_number = randint(0, 49)
     return random_number
 
 resul_gem = gen_number()
 print(resul_gem)
+
+def description():
+    data = gameData.data[0]
+    compare_name = data['name']
+    compare_descrip = data['description']
+    compare_contry = data['country']
+    compar_foll = data['follower_count']
+    return f"{compare_name}, a {compare_descrip}, from {compare_contry} with {compar_foll} followers"
+
+result = description()
+# print(gameData.data[0])
+print(result)
