12f916c5421ce3da403947fb54b360223ab01b9e
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-11-05 20:14:42 -0300

    description with random number

--- classes/102.2-higherLower.py
+++ classes/102.2-higherLower.py
@@ -8,16 +8,16 @@ def gen_number():
     return random_number
 
 resul_gem = gen_number()
-print(resul_gem)
+# print(resul_gem)
 
-def description():
-    data = gameData.data[0]
+def description(chosen):
+    data = gameData.data[chosen]
     compare_name = data['name']
     compare_descrip = data['description']
     compare_contry = data['country']
     compar_foll = data['follower_count']
     return f"{compare_name}, a {compare_descrip}, from {compare_contry} with {compar_foll} followers"
 
-result = description()
+result = description(resul_gem)
 # print(gameData.data[0])
 print(result)
