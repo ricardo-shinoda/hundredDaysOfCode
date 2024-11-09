57ac8579e321b86cfa6a14ed9a1deb55b76b6ef7
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-11-08 21:32:53 -0300

    add questioning

--- classes/102.2-higherLower.py
+++ classes/102.2-higherLower.py
@@ -7,8 +7,10 @@ def gen_number():
     random_number = randint(0, 49)
     return random_number
 
+
 resul_gem = gen_number()
 # print(resul_gem)
+
 
 def description(chosen):
     data = gameData.data[chosen]
@@ -18,6 +20,29 @@ def description(chosen):
     compar_foll = data['follower_count']
     return f"{compare_name}, a {compare_descrip}, from {compare_contry} with {compar_foll} followers"
 
+
 result = description(resul_gem)
 # print(gameData.data[0])
 print(result)
+
+
+def generationg_new():
+    result(resul_gem)
+
+
+first_choice = generationg_new()
+second_choice = generationg_new()
+
+
+def question(a, b):
+    print("Who would you pick?")
+    # first = a
+    print(f"Would you pick a: {a}")
+    print("VS")
+    # second = input(b)
+    print(f"Would you pick : {b}")
+    return
+
+
+questioning = question(a=first_choice, b=second_choice)
+print(questioning)
