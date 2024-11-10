3d675ac646762e36d3f2c6e1607f8aa6951a8112
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-11-10 18:54:01 -0300

    function

--- classes/102.2-higherLower.py
+++ classes/102.2-higherLower.py
@@ -1,48 +1,39 @@
 import random
 from gameData import data
 
+
+# found the random account information
 account_a = random.choice(data)
 account_b = random.choice(data)
 
 if account_a == account_a:
     account_b = random.choice(data)
 
-
-def description(chosen):
-    data = gameData.data[chosen]
-    compare_name = data['name']
-    compare_descrip = data['description']
-    compare_contry = data['country']
-    compar_foll = data['follower_count']
-    return f"{compare_name}, a {compare_descrip}, from {compare_contry} with {compar_foll} followers"
+# Formata DATA into desired readable format
 
 
-result = description(resul_gem)
-# print(gameData.data[0])
-print(result)
+def create_sentence(pick):
+    name = pick['name']
+    desc = pick['description']
+    country = pick['country']
+    followers = pick['follower_count']
+    return f'Compare a: {name}, from {country}, with {followers} followers.'
 
 
-def generationg_new():
-    response = result(resul_gem)
-    return response
+description1 = create_sentence(account_a)
+description2 = create_sentence(account_b)
+print(description1)
+print(description2)
+
+# def question(a, b):
+#     print("Who would you pick?")
+#     # first = a
+#     print(f"Would you pick a: {a}")
+#     print("VS")
+#     # second = input(b)
+#     print(f"Would you pick : {b}")
+#     return
 
 
-generator = generationg_new()
-
-
-first_choice = generator
-second_choice = generator
-
-
-def question(a, b):
-    print("Who would you pick?")
-    # first = a
-    print(f"Would you pick a: {a}")
-    print("VS")
-    # second = input(b)
-    print(f"Would you pick : {b}")
-    return
-
-
-questioning = question(a=first_choice, b=second_choice)
-print(questioning)
+# questioning = question(a=first_choice, b=second_choice)
+# print(questioning)
