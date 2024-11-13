5cbb7cabdcbaecb162469f9a85c82ffdb9a9caf1
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-11-13 20:59:13 -0300

    all todos defined

---
+++ CoffeeMachine/main.py
@@ -0,0 +1,24 @@
+
+
+# TODO: 1. Prompt user by asking "What would you like? (espresso/latte/cappuccnino): "
+
+# TODO: 2. Turn off the coffee machione by entering "off" to the prompt
+
+# TODO: 3. Print report
+
+'''
+When the user enters “report” to the prompt, a report should be generated that shows
+the current resource values. e.g.
+Water: 100ml
+Milk: 50ml
+Coffee: 76g
+Money: $2.5
+'''
+
+# TODO: 4. Check resources sufficient?
+
+# TODO: 5. Process coins
+
+# TODO: 6. Check transaction successful?
+
+# TODO: 7. Make Coffee
